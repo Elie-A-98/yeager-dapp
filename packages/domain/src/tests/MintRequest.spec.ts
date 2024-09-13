2834316f174f0a221276e6f32259c71cb07e794e
Elie <elieatamech@hotmail.com>
2024-09-13 20:03:16 +0300

    test: #202 added unit tests for the domain package

---
+++ packages/domain/src/tests/MintRequest.spec.ts
@@ -0,0 +1,46 @@
+import { expect } from "chai";
+import { mock, anything, when, instance, verify } from "ts-mockito";
+import { IMinter } from "../minting/IMinter.js";
+import { Metadata, MintRequest } from "../minting/MintRequest.js";
+import { BusinessValidationError } from "../BusinessValidationError.js";
+
+describe("Minting", () => {
+  describe("MintRequest", () => {
+    let mockedMinter: IMinter = mock<IMinter>();
+    let addr1 = "address1";
+
+    it("should fail if the address is not allowed to mint", () => {
+      // Arrange
+      when(mockedMinter.canMint(anything())).thenReturn(false);
+      let minter = instance(mockedMinter);
+      const metadata: Metadata = {
+        name: "TestName",
+        description: "TestFile",
+        file: new Blob([]),
+      };
+
+      // Act + Assert
+      expect(() => MintRequest.CreateNew(addr1, metadata, minter)).throw(
+        BusinessValidationError
+      );
+      verify(mockedMinter.canMint(anything())).called();
+    });
+
+    it("should succeed if the address is allowed to mint", () => {
+      // Arrange
+      when(mockedMinter.canMint(anything())).thenReturn(true);
+      let minter = instance(mockedMinter);
+      const metadata: Metadata = {
+        name: "TestName",
+        description: "TestFile",
+        file: new Blob([]),
+      };
+
+      // Act + Assert
+      expect(() => MintRequest.CreateNew(addr1, metadata, minter)).not.throw(
+        BusinessValidationError
+      );
+      verify(mockedMinter.canMint(anything())).called();
+    });
+  });
+});
