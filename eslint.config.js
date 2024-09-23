32b22a7e4a656e35c98f7eb2b56adcafb5e40b69
Elie <elieatamech@hotmail.com>
2024-09-24 01:31:00 +0300

    build: added turbo

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["backend/**/*.{js,mjs,cjs,ts}"]},
-  {files: ["packages/**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["backend/**/*.{js,mjs,cjs,ts}"] },
+  { files: ["packages/**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
