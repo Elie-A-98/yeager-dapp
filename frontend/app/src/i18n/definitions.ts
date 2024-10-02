8a65e5c428853bbacba9beb1efafbb378f05865b
Elie <elieatamech@hotmail.com>
2024-10-02 05:55:45 +0300

    fix: fixed dictionary

--- frontend/app/src/i18n/definitions.ts
+++ frontend/app/src/i18n/definitions.ts
@@ -47,70 +47,69 @@ export const dictionary = {
   en: BASE_LANG,
   fr: {
     'common.problem-occured': 'Un problème est survenu',
-    'common.min-length-3': 'Doit avoir une longueur minimale de 3 caractères',
-    'common.max-length-30': 'Doit avoir une longueur maximale de 30 caractères',
-    'common.required': 'Requis',
+    'common.min-length-3': 'Devrait avoir une longueur minimale de 3',
+    'common.max-length-30': 'Devrait avoir une longueur maximale de 30',
+    'common.required': 'Ceci est requis',
     'common.invalid-account-address': 'Adresse invalide',
     'common.connect-to-provider': 'Veuillez vous connecter à un fournisseur',
-    'common.who-can-mint': 'Puis-je en créer un ?',
+    'common.who-can-mint': 'Puis-je mint ?',
 
     'home.title': 'Bienvenue sur la DApp',
-    'home.description': 'Ceci est un projet Web3 full-stack.',
-    'home.source-code': 'Code source',
+    'home.description': 'Ceci est un projet Web3 full stack.',
+    'home.source-code': 'Code Source',
 
-    'who-can-mint.title': 'Qui peut en créer ?',
+    'who-can-mint.title': 'Qui peut mint ?',
     'who-can-mint.description':
-      "Pour l'instant, un compteur détermine si vous êtes autorisé à en créer un. Le compteur est incrémenté à chaque demande de création. Vous pouvez en créer un si le nombre % 2 = 0",
+      "Pour l'instant, il y a un compteur qui décide si vous pouvez mint. Le compteur est incrémenté à chaque fois que je reçois une demande de mint. Vous pouvez mint si le compteur % 2 = 0",
     'header.gallery': 'Galerie',
-    'header.mint': 'Créer', // Using "Créer" for a more action-oriented translation
+    'header.mint': 'Mint',
     'connect-wallet.cant-connect-to-metamask': 'Impossible de se connecter à MetaMask',
     'connect-wallet.metamask-pending':
-      "MetaMask semble être en attente. Veuillez ouvrir l'application MetaMask et vous connecter",
-    'gallery.title': "Voir votre galerie d'actifs",
-    'mint-request.title': "Demande de création d'un nouveau jeton",
+      'Il semble que MetaMask soit en attente. Veuillez ouvrir l’application MetaMask et vous connecter',
+    'gallery.title': 'Voir votre galerie d’actifs',
+    'mint-request.title': 'Demande pour mint un nouveau token',
     'mint-request.form.name': 'Nom',
     'mint-request.form.description': 'Description',
     'mint-request.form.asset': 'Actif',
-    'mint-request.success': 'Jeton créé !',
-    'transfer-asset.title': 'Transférer votre actif',
-    'page-not-found.title': 'Page introuvable',
-
-    'wrong-network.prompt': `Veuillez vous connecter au bon réseau. L'identifiant de chaîne doit être${CHAIN_ID}`,
-  'token-details.noot-yours': `This asset is not yours so you can't transfer it`
-
+    'mint-request.success': 'Token Minté !',
+    'transfer-asset.title': 'Transférez votre actif',
+    'page-not-found.title': 'Page non trouvée',
+    'wrong-network.prompt':
+      'Veuillez vous connecter au bon réseau. L’identifiant de chaîne doit être ${CHAIN_ID}',
+    'token-details.noot-yours': "Cet actif n'est pas à vous, donc vous ne pouvez pas le transférer"
   },
   it: {
     'common.problem-occured': 'Si è verificato un problema',
-    'common.min-length-3': 'Deve avere una lunghezza minima di 3 caratteri',
-    'common.max-length-30': 'Deve avere una lunghezza massima di 30 caratteri',
-    'common.required': 'Obbligatorio',
+    'common.min-length-3': 'Dovrebbe avere una lunghezza minima di 3',
+    'common.max-length-30': 'Dovrebbe avere una lunghezza massima di 30',
+    'common.required': 'Questo è richiesto',
     'common.invalid-account-address': 'Indirizzo non valido',
-    'common.connect-to-provider': 'Collegati a un provider',
-    'common.who-can-mint': 'Posso crearne uno?', // Using "crearne uno" for clarity
+    'common.connect-to-provider': 'Si prega di connettersi a un provider',
+    'common.who-can-mint': 'Posso fare il mint?',
 
-    'home.title': 'Benvenuto su DApp',
-    'home.description': 'Questo è un progetto Web3 full-stack.',
-    'home.source-code': 'Codice sorgente',
+    'home.title': 'Benvenuto nella DApp',
+    'home.description': 'Questo è un progetto Web3 full stack.',
+    'home.source-code': 'Codice Sorgente',
 
-    'who-can-mint.title': 'Chi può crearne uno?',
+    'who-can-mint.title': 'Chi può fare il mint?',
     'who-can-mint.description':
-      'Per ora, un contatore determina se sei autorizzato a crearne uno. Il contatore viene incrementato a ogni richiesta di creazione. Puoi crearne uno se numero % 2 = 0',
+      "Per ora c'è un contatore che decide se puoi fare il mint. Il contatore viene incrementato ogni volta che ricevo una richiesta di mint. Puoi fare il mint se contatore % 2 = 0",
     'header.gallery': 'Galleria',
-    'header.mint': 'Crea', // Using "Crea" for a more action-oriented translation
+    'header.mint': 'Mint',
     'connect-wallet.cant-connect-to-metamask': 'Impossibile connettersi a MetaMask',
     'connect-wallet.metamask-pending':
-      "Sembra che MetaMask sia in attesa. Apri l'app MetaMask e connettiti",
+      'Sembra che MetaMask sia in attesa. Si prega di aprire l’app MetaMask e connettersi',
     'gallery.title': 'Visualizza la tua galleria di asset',
-    'mint-request.title': 'Richiesta di creazione di un nuovo token',
+    'mint-request.title': 'Richiesta di mint di un nuovo token',
     'mint-request.form.name': 'Nome',
     'mint-request.form.description': 'Descrizione',
     'mint-request.form.asset': 'Asset',
-    'mint-request.success': 'Token creato!',
+    'mint-request.success': 'Token Mintato!',
     'transfer-asset.title': 'Trasferisci il tuo asset',
     'page-not-found.title': 'Pagina non trovata',
-    'wrong-network.prompt': `Si prega di connettersi alla rete giusta. L'ID della catena deve essere ${CHAIN_ID}`,
-  'token-details.noot-yours': `This asset is not yours so you can't transfer it`
-
+    'wrong-network.prompt':
+      "Si prega di connettersi alla rete corretta. L'ID della catena deve essere ${CHAIN_ID}",
+    'token-details.noot-yours': 'Questo asset non è tuo, quindi non puoi trasferirlo'
   }
   // will ensure we have all the sentences implemented in all the languages that we support
 } as const satisfies Record<Languages, Record<keyof typeof BASE_LANG, string>>
